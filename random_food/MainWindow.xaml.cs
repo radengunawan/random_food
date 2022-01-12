c7b13d29c6c00df75f1c915e72fc99159af78e93
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-12 10:34:04 +0800

    Now, objects are involved

--- random_food/MainWindow.xaml.cs
+++ random_food/MainWindow.xaml.cs
@@ -50,30 +50,45 @@ namespace random_food
             //Relationship between XAML - .CS 
 
             // REPLACED BY THIS
-            TextBlock[] item_xaml = new TextBlock[] { item1, item2, item3, item4, item5};
-            TextBlock[] price_xaml = new TextBlock[] { price1, price2, price3, price4, price5};
+            TextBlock[] item_xaml_list = new TextBlock[] { item1, item2, item3, item4, item5};
+            TextBlock[] price_xaml_list = new TextBlock[] { price1, price2, price3, price4, price5};
 
-            for (int i = 0; i < item_xaml.Length; i++)
+            string [] item_real_list = new string[item_xaml_list.Length];
+            float[] price_real_list = new float[price_xaml_list.Length];
+            Order[] list_of_order = new Order[price_xaml_list.Length];
+
+            float sum = 0;
+
+            for (int i = 0; i < item_xaml_list.Length; i++)
             {
-                item_xaml[i].Text = menuItems[i].Description;
-                price_xaml[i].Text = menuItems[i].Price;
+                item_xaml_list[i].Text = menuItems[i].Description;
+                price_xaml_list[i].Text = menuItems[i].Price;
+
+                //STORE the real back-end (not only visual)
+                item_real_list[i] = menuItems[i].Description;
+
+                //price_real_list[i] = float.Parse(menuItems[i].Price );
+                price_real_list[i] = 5.50F;
+
+
+                // Assembly in a dedicated object is better (more concesiful):
+                list_of_order[i] = new Order { order_item = menuItems[i].Description, order_price = price_real_list[i] };
+
             }
 
-            // LAST: 
-            //item1.Text = menuItems[0].Description;
-            //price1.Text = menuItems[0].Price;
+            foreach (Order oo in list_of_order)
+            {
+                if (oo.order_price != null)
+                {
+                    sum += oo.order_price;
+                }
+                else
+                {
+                    continue;
+                }
+            }
 
-            //item2.Text = menuItems[1].Description;
-            //price2.Text = menuItems[1].Price;
-
-            //item3.Text = menuItems[2].Description;
-            //price3.Text = menuItems[2].Price;
-
-            //item4.Text = menuItems[3].Description;
-            //price4.Text = menuItems[3].Price;
-
-            //item5.Text = menuItems[4].Description;
-            //price5.Text = menuItems[4].Price;
+            priceTextBox.Text = sum.ToString();
 
             MenuItem special_the = new MenuItem()
             {
@@ -86,12 +101,24 @@ namespace random_food
 
             item6.Text = special_the.Description;
             price6.Text = special_the.Price;
+            //list_of_order[5] = new Order { order_item = item6.Text, order_price = 5.50F };
 
             MenuItem guacamole_based = new MenuItem();
             guacamole_based.GenerateMenuItem();
 
             guacamolePrice = guacamole_based.Price;
             guacamole.Text = "Add guacamole for " + guacamolePrice;
+            //list_of_order[6] = new Order { order_item = "guacamole", order_price = 5.50F };
+
+            //foreach (Order oo in list_of_order)
+            //{
+            //    if (oo != null)
+            //    {
+            //        sum += oo.order_price;
+            //    }
+                
+            //}
+
         }//end MakeTheMenu
 
         private void Repeat_Btn_Click(object sender, RoutedEventArgs e)
---
+++ random_food/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random_food
+{
+    class Order
+    {
+        public string order_item;
+        public float order_price;
+
+        //public void SingleOrder()
+        //{
+
+        //}
+    }
+}
