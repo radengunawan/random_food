52bed9de6c060226e2db6d8bf43580e7823eb1c8
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-12 16:17:36 +0800

    now sum is correct, and able to add (when checked) and subtracted. NEXT (to be fixed): update values every re-order

--- random_food/MainWindow.xaml.cs
+++ random_food/MainWindow.xaml.cs
@@ -27,17 +27,27 @@ namespace random_food
         } //end main window
 
 
+        List<Order> list_of_order = new List<Order>();
+        MenuItem[] menuItems = new MenuItem[5];
+        string guacamolePrice;
+        
+        //string [] item_real_list = new string[item_xaml_list.Length+2];
+        List<string> item_real_list = new List<String>();
+
+        //float[] price_real_list = new float[price_xaml_list.Length+2];
+        List<float> price_real_list = new List<float>();
+
+        float sum = 0;
 
         private void MakeTheMenu()
         {
-            MenuItem[] menuItems = new MenuItem[5];
-            string guacamolePrice;
 
             for (int i = 0; i < 5; i++)
             {
                 menuItems[i] = new MenuItem();
 
-                if (i >= 3) {
+                if (i >= 3)
+                {
 
                     menuItems[i].Breads = new string[] { "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel" };
 
@@ -46,6 +56,8 @@ namespace random_food
                 menuItems[i].GenerateMenuItem();
 
             } //end loop
+            //MenuItem[] menuItems = new MenuItem[5];
+            
 
             //Relationship between XAML - .CS 
 
@@ -56,17 +68,17 @@ namespace random_food
             List<TextBlock> item_xaml_list = new List<TextBlock> { item1, item2, item3, item4, item5 };
             List<TextBlock> price_xaml_list = new List<TextBlock> { price1, price2, price3, price4, price5 };
 
-            //string [] item_real_list = new string[item_xaml_list.Length+2];
-            List<string> item_real_list = new List<String>();
+            ////string [] item_real_list = new string[item_xaml_list.Length+2];
+            //List<string> item_real_list = new List<String>();
 
-            //float[] price_real_list = new float[price_xaml_list.Length+2];
-            List<float> price_real_list = new List<float>();
+            ////float[] price_real_list = new float[price_xaml_list.Length+2];
+            //List<float> price_real_list = new List<float>();
 
 
             //Order[] list_of_order = new Order[price_xaml_list.Length+2];
-            List<Order> list_of_order = new List<Order>();
+            // List<Order> list_of_order = new List<Order>();
 
-            float sum = 0;
+            //float sum = 0; placed outward
             // Console.WriteLine(list_of_order.Length);
            
 
@@ -95,7 +107,7 @@ namespace random_food
                 //list_of_order[i] = new Order { order_item = menuItems[i].Description, order_price = price_real_list[i] };
 
                 //if using list:
-                list_of_order.Add(new Order { order_item = menuItems[i].Description, order_price = price_real_list[i] });
+               // list_of_order.Add(new Order { order_item = menuItems[i].Description, order_price = price_real_list[i] });
             }
             //Console.WriteLine(list_of_order.Count);
 
@@ -114,6 +126,9 @@ namespace random_food
             price6.Text = special_the.Price;
             //list_of_order[5] = new Order { order_item = item6.Text, order_price = 5.50F };
             list_of_order.Add(new Order { order_item = item6.Text, order_price = float.Parse(price6.Text.Split('$')[1]) });
+            
+            item_real_list.Add(price6.Text);
+            price_real_list.Add(float.Parse(price6.Text.Split('$')[1]));
 
             MenuItem guacamole_based = new MenuItem();
             guacamole_based.GenerateMenuItem();
@@ -123,18 +138,17 @@ namespace random_food
             //list_of_order[6] = new Order { order_item = "guacamole", order_price = 5.50F };
             list_of_order.Add(new Order { order_item = "guacamole", order_price = float.Parse(guacamolePrice.Split('$')[1]) });
 
-            foreach (Order oo in list_of_order)
-            {
-                //if (oo != null)
-               // {
-                    sum += oo.order_price;
-               // }
-
-            }
+            item_real_list.Add("guacamole");
+            price_real_list.Add(float.Parse(guacamolePrice.Split('$')[1]));
             priceTextBox.Text = sum.ToString();
-            Console.WriteLine(list_of_order.Count);
+            //foreach (Order oo in list_of_order)
+            //{
 
-          
+            //        sum += oo.order_price;
+
+
+            //}
+            //priceTextBox.Text = sum.ToString();
 
         }//end MakeTheMenu
 
@@ -142,5 +156,172 @@ namespace random_food
         {
             MakeTheMenu();
         }
+
+        private void c1_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = menuItems[0].Description, order_price = price_real_list[0] });
+            sum += price_real_list[0];
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c2_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = menuItems[1].Description, order_price = price_real_list[1] });
+            sum += price_real_list[1];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+
+        private void c3_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = menuItems[2].Description, order_price = price_real_list[2] });
+
+            sum += price_real_list[2];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c4_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = menuItems[3].Description, order_price = price_real_list[3] });
+
+            sum += price_real_list[3];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c5_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = menuItems[4].Description, order_price = price_real_list[4] });
+
+            sum += price_real_list[4];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c6_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = item_real_list[5], order_price = price_real_list[5] });
+
+            sum += price_real_list[5];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c_bonus_Checked(object sender, RoutedEventArgs e)
+        {
+            list_of_order.Add(new Order { order_item = item_real_list[6], order_price = price_real_list[6] });
+
+            sum += price_real_list[6];
+
+            priceTextBox.Text = sum.ToString();
+        }
+
+        private void c1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == menuItems[0].Description)
+                {
+                    sum -= price_real_list[0];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == menuItems[1].Description)
+                {
+                    sum -= price_real_list[1];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c3_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == menuItems[2].Description)
+                {
+                    sum -= price_real_list[2];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c4_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == menuItems[3].Description)
+                {
+                    sum -= price_real_list[3];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c5_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == menuItems[4].Description)
+                {
+                    sum -= price_real_list[4];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c6_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == item_real_list[5])
+                {
+                    sum -= price_real_list[5];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void c_bonus_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (Order item in list_of_order)
+            {
+                if (item.order_item == item_real_list[6])
+                {
+                    sum -= price_real_list[6];
+                    priceTextBox.Text = sum.ToString();
+                    list_of_order.Remove(item);
+                    break;
+                }
+            }
+        }
+
+
+
+        //private void c1_Unchecked(object sender, RoutedEventArgs e)
+        //{
+        //    foreach (Author item in a1)
+        //}
     } //end MainWindow
 } //end namespace
